28ba2e134654ad37c05c2a539296e8996ef409ac
Daniel Snider <qtipkk@gmail.com>
2011-06-27 02:38:03 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='rendermath',
+      version='1.0',
+      description="LaTeX math to png renderer",
+      author="Daniel Snider",
+      author_email="qtipkk@gmail.com",
+      py_modules=['rendermath'],
+      keywords="latex math render png image",
+      )
+
